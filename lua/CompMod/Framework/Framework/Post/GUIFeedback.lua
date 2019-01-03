f3af901d14f7774611a548a800e60ddb37d0d838
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-01-03 03:20:47 +0000

    wh00ps

--- lua/CompMod/Framework/Framework/Post/GUIFeedback.lua
+++ lua/CompMod/Framework/Framework/Post/GUIFeedback.lua
@@ -5,7 +5,7 @@ if _G[kModName].config.kShowInFeedbackText then
 	originalFeedbackInit = Class_ReplaceMethod("GUIFeedback", "Initialize",
 	function(self)
 		originalFeedbackInit(self)
-		self.buildText:SetText(self.buildText:GetText() .. " (" .. kModName .. " " .. _G[kModName]:GetVersion() .. ")")
+		self.buildText:SetText(self.buildText:GetText() .. " (" .. _G[kModName].config.kModName .. " " .. _G[kModName]:GetVersion() .. ")")
 	end)
 
 end
