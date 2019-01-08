090cbcecd38bfbab3c3107027265bde40784bd43
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-01-08 20:15:04 +0000

    add hmg change

--- lua/CompMod/Marine/Weapons/HMG/Post/Balance.lua
+++ lua/CompMod/Marine/Weapons/HMG/Post/Balance.lua
@@ -1,4 +1,5 @@
 kHeavyMachineGunCost = 25 -- lowered from 20
 kHeavyMachineGunDropCost = 25
 kHeavyMachineGunTechResearchTime = 20 -- lowered from 30
-kHeavyMachineGunDamage = 8
+kHeavyMachineGunDamage = 12
+kHeavyMachineGunDamageType = kDamageType.StructuresOnlyLight
