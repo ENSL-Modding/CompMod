0691f36f6115cfca4ad110c4880344c1bd786f71
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-01-09 16:37:18 +0000

    add walljump b00st change

--- lua/CompMod/Alien/Lifeforms/Skulk/Post/Skulk.lua
+++ lua/CompMod/Alien/Lifeforms/Skulk/Post/Skulk.lua
@@ -1,1 +1,34 @@
-Skulk.kMaxSpeed = 7.5 
+Skulk.kMaxSpeed = 7.5
+
+function Skulk:ModifyJump(input, velocity, jumpVelocity)
+
+    if self:GetCanWallJump() then
+
+        local direction = input.move.z == -1 and -1 or 1
+
+        -- we add the bonus in the direction the move is going
+        local viewCoords = self:GetViewAngles():GetCoords()
+        self.bonusVec = viewCoords.zAxis * direction
+        self.bonusVec.y = 0
+        self.bonusVec:Normalize()
+
+        jumpVelocity.y = 3 + math.min(1, 1 + viewCoords.zAxis.y) * 2
+
+        local fraction = 1 - Clamp( velocity:GetLengthXZ() / self:GetMaxWallJumpSpeed(), 0, 1)
+
+        local force = math.max(Skulk.kMinWallJumpForce, Skulk.kWallJumpForce * fraction)
+
+        self.bonusVec:Scale(force)
+
+        if not self:GetRecentlyWallJumped() or fraction > 0.1 then
+
+            self.bonusVec.y = viewCoords.zAxis.y * Skulk.kVerticalWallJumpForce
+            jumpVelocity:Add(self.bonusVec)
+
+        end
+
+        self.timeLastWallJump = Shared.GetTime()
+
+    end
+
+end
