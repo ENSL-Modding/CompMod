6c719d3c524858e150648a5f20db13e89da609c1
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-01-10 18:18:51 +0000

    build 2

--- lua/CompMod/Config.lua
+++ lua/CompMod/Config.lua
@@ -4,7 +4,7 @@ function GetModConfig(kLogLevels)
 	config.kLogLevel = kLogLevels.info
 	config.kShowInFeedbackText = true
 	config.kModVersion = "1"
-	config.kModBuild = "1"
+	config.kModBuild = "2"
 
 	config.modules =
 	{
