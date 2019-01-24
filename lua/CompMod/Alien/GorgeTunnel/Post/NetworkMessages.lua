a54c81687abec81abfb94e9421858f8fc9007114
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-01-24 19:53:26 +0000

    apparently you can do this now

--- lua/CompMod/Alien/GorgeTunnel/Post/NetworkMessages.lua
+++ lua/CompMod/Alien/GorgeTunnel/Post/NetworkMessages.lua
@@ -1,22 +1,3 @@
--- the gorgebuildmessage is registered on script load using a local variable that is declared in the same file
--- so r1p changing it, time to be hacky
-
-local kRandomVector = Vector(0, -1000, 0)
-
-local oldBuildGorgeDropStructureMessage = BuildGorgeDropStructureMessage
-function BuildGorgeDropStructureMessage(origin, direction, structureIndex, lastClickedPosition)
-  if structureIndex == 6 then
-    structureIndex = 5
-    lastClickedPosition = kRandomVector
-  end
-  return oldBuildGorgeDropStructureMessage(origin, direction, structureIndex, lastClickedPosition)
-end
-
-local oldParseGorgeBuildMessage = ParseGorgeBuildMessage
-function ParseGorgeBuildMessage(t)
-  if t.lastClickedPosition == kRandomVector and t.structureIndex == 5 then
-    t.lastClickedPosition = Vector(0,0,0)
-    t.structureIndex = 6
-  end
-  return oldParseGorgeBuildMessage(t)
-end
+local kGorgeBuildStructureMessage = Shared.GetNetworkMessageDefinition("GorgeBuildStructure")
+kGorgeBuildStructureMessage.structureIndex = "integer (1 to 6)"
+Shared.RegisterNetworkMessage("GorgeBuildStructure", kGorgeBuildStructureMessage)
