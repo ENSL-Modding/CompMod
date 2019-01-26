3cf34bd75acb2c54c536024a33766262dadfc9a2
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-01-26 22:09:56 +0000

    fix marine walk making sound maybe

--- lua/CompMod/Marine/Walk/Shared/MarineWalk.lua
+++ lua/CompMod/Marine/Walk/Shared/MarineWalk.lua
@@ -9,3 +9,23 @@ local networkVars = { }
 AddMixinNetworkVars(WalkMixin, networkVars)
 
 Shared.LinkClassToMap("Marine", Marine.kMapName, networkVars, true)
+
+if Client then
+  local oldTriggerFootstep = Player.TriggerFootstep
+  function Player:TriggerFootstep()
+    
+    if HasMixin(self, "Walk") and self:GetWalking() then
+      return
+    end
+
+    return oldTriggerFootstep(self)
+  end
+end
+
+local oldGetPlayFootsteps = Player.GetPlayFootsteps
+function Player:GetPlayFootsteps()
+    if not Client then
+        return false
+    end
+    return oldGetPlayFootsteps(self) and not (HasMixin(self, "Walk") and self:GetWalking())
+end
