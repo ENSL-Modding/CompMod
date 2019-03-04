818fae394560e64bf05674ffb2ede24e44c9a5c4
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-03-04 19:56:34 +0000

    increase skulk bitecone

--- lua/CompMod/Alien/Lifeforms/Skulk/Post/Weapons/Alien/BiteLeap.lua
+++ lua/CompMod/Alien/Lifeforms/Skulk/Post/Weapons/Alien/BiteLeap.lua
@@ -1,9 +1,9 @@
 function BiteLeap:GetMeleeBase()
     -- Width of box, height of box
-    return 0.7, 1
+    return 0.8, 1.1
 end
 
-local kRange = 1.62
+local kRange = 1.52
 
 CompMod:ReplaceLocal(BiteLeap.GetRange, "kRange", kRange)
 CompMod:ReplaceLocal(BiteLeap.OnTag, "kRange", kRange)
