5b01e8b953a1c0025d76201a653ded83047ef532
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-04-18 14:41:08 +0100

    remove axe fix

--- lua/CompMod/Config.lua
+++ lua/CompMod/Config.lua
@@ -69,7 +69,6 @@ function GetModConfig(kLogLevels)
 		"Global/ReadyRoomPanels",
 
 		-- Marine Changes
-		"Marine/AxeFix",
 		"Marine/GunDropTime",
 		"Marine/Jetpack",
 		"Marine/MarineClassChanges",
