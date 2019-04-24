7ff35863e2c6b4f82635a6ac16ce0f17e37992d1
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-04-24 21:30:24 +0100

    b2.1

--- lua/CompMod/Config.lua
+++ lua/CompMod/Config.lua
@@ -4,7 +4,7 @@ function GetModConfig(kLogLevels)
 	config.kLogLevel = kLogLevels.info
 	config.kShowInFeedbackText = true
 	config.kModVersion = "2"
-	config.kModBuild = "0.0"
+	config.kModBuild = "1.0"
 	config.disableRanking = true
 	config.use_config = "client"
 	config.techIdsToAdd = {
