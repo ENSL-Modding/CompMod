66140dd3b37cc8a4caac72293704398782a5ee79
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-05-27 22:47:20 +0100

    revert nutrient mist to vanilla

--- lua/CompMod/Commander/Alien/NutrientMist/Post/Balance.lua
+++
@@ -1,3 +0,0 @@
-kNutrientMistCooldown = 1 -- decreased from 2
-
-kNutrientMistPercentageIncrease = 50 -- lowered from 66
--- lua/CompMod/Config.lua
+++ lua/CompMod/Config.lua
@@ -27,7 +27,6 @@ function GetModConfig(kLogLevels)
 		-- Alien Commander
 		"Commander/Alien/Consume",
 		"Commander/Alien/LifeformEggs",
-		"Commander/Alien/NutrientMist",
 		"Commander/Alien/SupplyChanges",
 
 		-- Marine Commander
