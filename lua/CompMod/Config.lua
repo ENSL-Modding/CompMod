e6ba2eec5edc485a5c4d12fe22fcd3e5081b8cc3
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-05-27 22:44:39 +0100

    revert HMG to vanilla

--- lua/CompMod/Config.lua
+++ lua/CompMod/Config.lua
@@ -55,7 +55,6 @@ function GetModConfig(kLogLevels)
 
 		-- Weapons
 		"Marine/Weapons/Grenades",
-		"Marine/Weapons/HMG",
 		"Marine/Weapons/Mine",
 	}
 
--- lua/CompMod/Marine/Weapons/HMG/Post/Balance.lua
+++
@@ -1,5 +0,0 @@
-kHeavyMachineGunCost = 25 -- lowered from 20
-kHeavyMachineGunDropCost = 25
-kHeavyMachineGunTechResearchTime = 20 -- lowered from 30
-kHeavyMachineGunDamage = 12
-kHeavyMachineGunDamageType = kDamageType.StructuresOnlyLight
