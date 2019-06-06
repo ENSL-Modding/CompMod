f409f5d059482429509b7cf134fca16f48775f58
turtsmcgurts <slizizy@gmail.com>
2019-06-06 14:34:16 -0400

    Moved umbra to biomass 6

---
+++ lua/CompMod/Alien/Lifeforms/Lerk/Shared/LerkAdjustments.lua
@@ -0,0 +1,5 @@
+-- move umbra requirement to bio6
+CompMod:ChangeResearch(kTechId.Umbra, kTechId.BioMassSix, kTechId.None, kTechId.AllAliens)
+
+-- update techtree
+CompMod:ChangeAlienTechmapTech(kTechId.Umbra, 8, 8)
