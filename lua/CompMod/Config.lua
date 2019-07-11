34d3859843ca6a1476ebebabf1b0590fa94ca683
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-07-11 11:12:37 +0100

    leave the poor version number alone

--- lua/CompMod/Config.lua
+++ lua/CompMod/Config.lua
@@ -4,7 +4,7 @@ function GetModConfig(kLogLevels)
 	config.kLogLevel = kLogLevels.info
 	config.kShowInFeedbackText = true
 	config.kModVersion = "2"
-	config.kModBuild = "7.0-beta"
+	config.kModBuild = "6.0-beta"
 	config.disableRanking = true
 	config.use_config = "none"
 	config.techIdsToAdd = {
