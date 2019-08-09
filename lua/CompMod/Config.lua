0323ddd3c0c859e03ae5de5a93bcb7e6163d4f48
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-08-09 04:32:51 +0100

    update version

--- lua/CompMod/Config.lua
+++ lua/CompMod/Config.lua
@@ -103,7 +103,7 @@ function GetModConfig(kLogLevels)
 	config.kLogLevel = kLogLevels.info
 	config.kShowInFeedbackText = true
 	config.kModVersion = "2"
-	config.kModBuild = "7.2"
+	config.kModBuild = "7.3"
 	config.disableRanking = true
 	config.use_config = "none"
 
