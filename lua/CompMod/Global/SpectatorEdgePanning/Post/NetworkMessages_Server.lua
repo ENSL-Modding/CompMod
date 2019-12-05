855a55736a03d51ca2699de8b146483cc05d2e49
xGolden <brownchrisg@gmail.com>
2019-12-04 20:17:53 -0500

    Enable/Disable OverheadMove for spectators when following a player in overhead mode (#47)

---
+++ lua/CompMod/Global/SpectatorEdgePanning/Post/NetworkMessages_Server.lua
@@ -0,0 +1,19 @@
+local function onSpectatePlayer(client, message)
+
+    local spectatorPlayer = client:GetControllingPlayer()
+    if spectatorPlayer then
+
+        -- This only works for players on the spectator team.
+        if spectatorPlayer:GetTeamNumber() == kSpectatorIndex then
+            client:GetControllingPlayer():SelectEntity(message.entityId)
+            if message.entityId == Entity.invalidId then
+                spectatorPlayer:SetOverheadMoveEnabled(true)
+            else
+                spectatorPlayer:SetOverheadMoveEnabled(false)
+            end
+        end
+
+    end
+
+end
+Server.HookNetworkMessage("SpectatePlayer", onSpectatePlayer)
