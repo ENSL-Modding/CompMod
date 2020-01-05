bac61afa291a1ce23b3baa3bea030042161e7e43
xGolden <brownchrisg@gmail.com>
2020-01-05 02:40:09 -0500

    Update documentation

--- lua/CompMod/Config.lua
+++ lua/CompMod/Config.lua
@@ -110,7 +110,7 @@ function GetModConfig(kLogLevels)
 	config.kLogLevel = kLogLevels.info
 	config.kShowInFeedbackText = true
 	config.kModVersion = "2"
-	config.kModBuild = "9.0"
+	config.kModBuild = "10.0"
 	config.disableRanking = true
 	config.use_config = "none"
 
