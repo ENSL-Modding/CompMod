cc6305aa72a7beca2ae22d1f1598e8a11d1857a8
xGolden <brownchrisg@gmail.com>
2020-01-08 19:46:54 -0500

    Refactor Fade blink code to mod format

---
+++ lua/CompMod/Alien/Lifeforms/Fade/Blink/Shared/Fade.lua
@@ -0,0 +1,106 @@
+local kMaxSpeed = 6.2
+local kBlinkMaxSpeed = 25
+local kFadeGravityMod = 1.0
+local kFadeScanDuration = 4
+
+local networkVars =
+{
+    isScanned = "boolean",
+    shadowStepping = "compensated boolean",
+    timeShadowStep = "private compensated time",
+    shadowStepDirection = "private compensated vector",
+    shadowStepSpeed = "private compensated interpolated float",
+
+    etherealStartTime = "private time",
+    etherealEndTime = "private time",
+
+    -- True when we're moving quickly "through the ether"
+    ethereal = "compensated boolean",
+
+    landedAfterBlink = "private compensated boolean",
+
+    timeMetabolize = "private compensated time",
+
+    timeOfLastPhase = "time",
+}
+
+function Fade:OnCreate()
+
+    InitMixin(self, BaseMoveMixin, { kGravity = Player.kGravity * kFadeGravityMod })
+    InitMixin(self, GroundMoveMixin)
+    InitMixin(self, JumpMoveMixin)
+    InitMixin(self, CrouchMoveMixin)
+    InitMixin(self, CelerityMixin)
+    InitMixin(self, CameraHolderMixin, { kFov = kFadeFov })
+
+    Alien.OnCreate(self)
+
+    InitMixin(self, DissolveMixin)
+    InitMixin(self, TunnelUserMixin)
+    InitMixin(self, BabblerClingMixin)
+    InitMixin(self, FadeVariantMixin)
+
+    if Client then
+        InitMixin(self, RailgunTargetMixin)
+    end
+
+    self.shadowStepDirection = Vector()
+
+    if Server then
+
+        self.timeLastScan = 0
+        self.isBlinking = false
+        self.timeShadowStep = 0
+        self.shadowStepping = false
+
+    end
+
+    self.etherealStartTime = 0
+    self.etherealEndTime = 0
+    self.ethereal = false
+    self.landedAfterBlink = true
+
+end
+
+function Fade:GetMaxSpeed(possible)
+
+    if possible then
+        return kMaxSpeed
+    end
+
+    if self:GetIsBlinking() then
+        return kBlinkMaxSpeed
+    end
+
+    -- Take into account crouching.
+    return kMaxSpeed
+
+end
+
+function Fade:OnProcessMove(input)
+
+    Alien.OnProcessMove(self, input)
+
+    if Server then
+
+        if self.isScanned and self.timeLastScan + kFadeScanDuration < Shared.GetTime() then
+            self.isScanned = false
+        end
+
+    end
+
+    if not self:GetHasMetabolizeAnimationDelay() and self.previousweapon ~= nil and not self:GetIsBlinking() then
+        if self:GetActiveWeapon():GetMapName() == Metabolize.kMapName then
+            self:SetActiveWeapon(self.previousweapon)
+        end
+        self.previousweapon = nil
+    end
+
+end
+
+-- start the auto-crouch if player blinks and hold crouch for them
+function Fade:HandleButtons(input)
+
+    Alien.HandleButtons(self, input)
+
+end
