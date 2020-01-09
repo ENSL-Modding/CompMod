eccd4d327c73c27923cd5077a330c1c634b736d4
xGolden <brownchrisg@gmail.com>
2020-01-08 21:36:19 -0500

    Tick build number for 2.11.0

--- lua/CompMod/Config.lua
+++ lua/CompMod/Config.lua
@@ -116,7 +116,7 @@ function GetModConfig(kLogLevels)
 	config.kLogLevel = kLogLevels.info
 	config.kShowInFeedbackText = true
 	config.kModVersion = "2"
-	config.kModBuild = "10.0"
+	config.kModBuild = "11.0"
 	config.disableRanking = true
 	config.use_config = "none"
 
