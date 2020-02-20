32a1a6e29152140ae2017bcb5c346e8fc5145d65
xGolden <brownchrisg@gmail.com>
2020-02-20 18:02:03 -0500

    Fixed losing adv swipe bug (#87)

--- lua/CompMod/Alien/Lifeforms/Fade/AdvancedSwipe/Post/DamageTypes.lua
+++ lua/CompMod/Alien/Lifeforms/Fade/AdvancedSwipe/Post/DamageTypes.lua
@@ -1,10 +1,8 @@
 local oldGetUpgradedDamage = NS2Gamerules_GetUpgradedDamage
 
 function NS2Gamerules_GetUpgradedDamage(attacker, doer, damage, damageType, hitPoint)
-    if doer.kMapName == "swipe" then
-        if GetHasTech(attacker, kTechId.AdvancedSwipe, true) then
-            return damage * kAdvancedSwipeDamageScalar
-        end
+    if doer.kMapName == "swipe" and attacker:GetHasThreeHives() then
+        return damage * kAdvancedSwipeDamageScalar
     end
 
     return oldGetUpgradedDamage(attacker, doer, damage, damageType, hitPoint)
