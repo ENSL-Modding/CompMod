01076b9d24b53cae158e4a0498fcaf813717304a
adsfgg <11364579+adsfgg@users.noreply.github.com>
2020-09-13 22:36:24 +0100

    Fix swipeblink death icon

--- src/lua/CompMod/Modules/Alien/Fade/AdvancedSwipe/Post/Weapons/Alien/SwipeBlink.lua
+++ src/lua/CompMod/Modules/Alien/Fade/AdvancedSwipe/Post/Weapons/Alien/SwipeBlink.lua
@@ -1,5 +1,6 @@
 function SwipeBlink:GetDeathIconIndex()
-    if GetHasTech(self:GetParent(), kTechId.AdvancedSwipe) then
+    -- if GetHasTech(self:GetParent(), kTechId.AdvancedSwipe) then
+    if self:GetParent():GetHasThreeHives() then
         return kDeathMessageIcon.Stab
     else
         return kDeathMessageIcon.Swipe
