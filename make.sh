c34c2bbe0a6182ee6a9cc93d29086172bc7199a4
adsfgg <11364579+adsfgg@users.noreply.github.com>
2020-09-16 22:59:20 +0100

    Update make.sh to match framework

--- make.sh
+++ make.sh
@@ -18,6 +18,10 @@ mkdir build
 printf "Copying src\n"
 cp -r src/* build/
 
+# Clear .docugen files
+printf "Clearing .docugen files\n"
+find build/lua/* -type f -name ".docugen" -delete > /dev/null
+
 # Copy license
 printf "Copying LICENSE\n"
 cp LICENSE build/ 2>/dev/null || printf "\e[93mWarning\e[0m: no LICENSE file found!\n"
