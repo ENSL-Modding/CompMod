d27694c03efc4e717dfecfcc15aac4a88e0f26a8
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-01-03 20:22:31 +0000

    Update changelog to show detailed log on revision change

--- src/lua/CompMod/Modules/Changelog/Post/Player_Client.lua
+++ src/lua/CompMod/Modules/Changelog/Post/Player_Client.lua
@@ -1,15 +1,19 @@
-kCompModRevisionKey = "compmod_revision"
-
-local kChangelogURL = "https://enslcompmod.github.io/CompMod/"
-local kChangelogTitle = "NSL Competitive Mod"
 local logger = CompMod:GetModule('logger')
-local revision = CompMod:GetModule('versioning'):GetRevision()
+local currentRevision = CompMod:GetModule('versioning'):GetRevision()
 
-local function showChangeLog()
+local kCompModRevisionKey = "compmod_revision"
+local kChangelogTitle = "NSL Competitive Mod"
+local kChangelogURL = "https://enslcompmod.github.io/CompMod/changelog"
+local kChangelogDetailURL = "https://enslcompmod.github.io/CompMod/revisions/revision" .. currentRevision .. ".md"
+
+local function showChangeLog(withDetail)
+    withDetail = withDetail or false
+    local url = withDetail and kChangelogDetailURL or kChangelogURL
+
     if Shine then
-        Shine:OpenWebpage(kChangelogURL, kChangelogTitle)
+        Shine:OpenWebpage(url, kChangelogTitle)
     elseif Client.GetIsSteamOverlayEnabled() then
-        Client.ShowWebpage(kChangelogURL)
+        Client.ShowWebpage(url)
     elseif CompMod then
         logger:PrintWarn("Couldn't open changelog because no webview is available")
     end
@@ -19,13 +23,11 @@ local oldOnInitLocalClient = Player.OnInitLocalClient
 function Player:OnInitLocalClient()
     oldOnInitLocalClient(self)
 
-    local oldRevision = Client.GetOptionInteger(kCompModRevisionKey, 0)
-    local currentRevision = revision or "0"
-    if currentRevision ~= oldRevision then
+    local oldRevision = Client.GetOptionInteger(kCompModRevisionKey, -1)
+    if currentRevision > oldRevision then
         Client.SetOptionInteger(kCompModRevisionKey, currentRevision)
-        showChangeLog()
+        showChangeLog(true)
     end
-
 end
 
 Event.Hook("Console_changelog", showChangeLog)
