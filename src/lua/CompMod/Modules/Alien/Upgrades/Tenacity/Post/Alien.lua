aa63f48724a7e37429529acd2ade1733498118fd
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-01-11 13:50:24 +0000

    Fix that alien heals were applied twice :/

--- src/lua/CompMod/Modules/Alien/Upgrades/Tenacity/Post/Alien.lua
+++ src/lua/CompMod/Modules/Alien/Upgrades/Tenacity/Post/Alien.lua
@@ -1,12 +1,10 @@
-local oldAlienAddHealth = LiveMixin.AddHealth -- Alien.AddHealth
-function Alien:AddHealth(health, playSound, noArmor, hideEffect, healer, useEHP)
-    assert(health >= 0)
-
+local oldModifyHeal = Alien.ModifyHeal
+function Alien:ModifyHeal(healTable)
+    oldModifyHeal(self, healTable)
+    
     if not self.innateRegen and GetHasTenacityUpgrade(self) then
         local shellLevel = self:GetShellLevel()
         local healMultiplier = 1 + (self:GetIsUnderFire() and kTenacityInCombatPercentage or kTenacityOutOfCombatPercentage) * (shellLevel/3)
-        health = health * healMultiplier
+        healTable.health = healTable.health * healMultiplier
     end
-
-    return oldAlienAddHealth(self, health, playSound, noArmor, hideEffect, healer, useEHP)
 end
