fa26df1e973a9b50d91b839b9c2745e79831ba7a
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-01-11 23:37:02 +0000

    Add fade metab while commanding fix

--- src/lua/CompMod_FileHooks.lua
+++ src/lua/CompMod_FileHooks.lua
@@ -1,2 +1,3 @@
 ModLoader.SetupFileHook("lua/GUIFeedback.lua", "lua/CompMod/GUI/GUIFeedback.lua", "post")
 ModLoader.SetupFileHook("lua/Armory.lua", "lua/CompMod/Structures/Marine/Armory.lua", "post")
+ModLoader.SetupFileHook("lua/CommandStructure_Server.lua", "lua/CompMod/Structures/CommandStructure_Server.lua")
