d4e58d8676fde7761206ad74066fd8dc8c3ca1b7
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-01-12 00:03:48 +0000

    Add rest of QOL changes

--- src/lua-old/CompMod/Modules/QOL/PreventMultipleInitialIPs/Post/BalanceMisc.lua
+++
@@ -1,3 +0,0 @@
--- Disable additional IPs
-
-kSecondInitialInfantryPortalMinPlayerCount = 9999 -- 9
--- src/lua/CompMod_FileHooks.lua
+++ src/lua/CompMod_FileHooks.lua
@@ -1,3 +1,10 @@
+ModLoader.SetupFileHook("lua/Marine.lua", "lua/CompMod/Classes/Marine/Marine.lua", "post")
+
 ModLoader.SetupFileHook("lua/GUIFeedback.lua", "lua/CompMod/GUI/GUIFeedback.lua", "post")
+ModLoader.SetupFileHook("lua/GUIPlayerStatus.lua", "lua/CompMod/GUI/GUIPlayerStatus.lua", "post")
+ModLoader.SetupFileHook("lua/GUIScoreboard.lua", "lua/CompMod/GUI/GUIScoreboard.lua", "post")
+
 ModLoader.SetupFileHook("lua/Armory.lua", "lua/CompMod/Structures/Marine/Armory.lua", "post")
-ModLoader.SetupFileHook("lua/CommandStructure_Server.lua", "lua/CompMod/Structures/CommandStructure_Server.lua")
+ModLoader.SetupFileHook("lua/CommandStructure_Server.lua", "lua/CompMod/Structures/CommandStructure_Server.lua", "post")
+
+ModLoader.SetupFileHook("lua/NS2ConsoleCommands_Server.lua", "lua/CompMod/NS2ConsoleCommands_Server.lua", "replace")
