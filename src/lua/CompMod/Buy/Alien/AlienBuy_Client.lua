c0bf6138372bda33c3cdbda92c54bff42854a4a2
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-01-30 08:35:44 +0000

    Tidy up AlienBuy_Client.lua

--- src/lua/CompMod/Buy/Alien/AlienBuy_Client.lua
+++ src/lua/CompMod/Buy/Alien/AlienBuy_Client.lua
@@ -1,8 +1,6 @@
 local gTierThreeTech
 function GetAlienTierThreeFor(techId)
-
     if not gTierThreeTech then
-
         gTierThreeTech = {}
 
         gTierThreeTech[kTechId.Skulk] = kTechId.Xenocide
@@ -10,9 +8,7 @@ function GetAlienTierThreeFor(techId)
         gTierThreeTech[kTechId.Lerk] = kTechId.Umbra
         gTierThreeTech[kTechId.Fade] = kTechId.AdvancedSwipe
         gTierThreeTech[kTechId.Onos] = kTechId.Stomp
-
     end
 
     return gTierThreeTech[techId]
-
 end
