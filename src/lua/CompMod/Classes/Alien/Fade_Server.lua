47aa5be982b2bb2b4f4fe3b69872dabc5890e2fa
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-02-13 11:04:57 +0000

    Refactor Fade metab fix; also reset when logging out of the Hive

--- src/lua/CompMod/Classes/Alien/Fade_Server.lua
+++ src/lua/CompMod/Classes/Alien/Fade_Server.lua
@@ -1,3 +1,21 @@
 function Fade:GetTierThreeTechId()
     return kTechId.AdvancedSwipe
 end
+
+local function ResetMetabolize(self)
+    if self.previousweapon ~= nil and self:GetActiveWeapon():GetMapName() == Metabolize.kMapName then
+        self:SetActiveWeapon(self.previousweapon)
+        self.previousweapon = nil
+
+        -- Clear metabolize cooldown
+        self.timeMetabolize = Shared.GetTime() - Fade.kMetabolizeAnimationDelay
+    end
+end
+
+function Fade:OnCommanderStructureLogin(_)
+    ResetMetabolize(self)
+end
+
+function Fade:OnCommanderStructureLogout(_)
+    ResetMetabolize(self)
+end
