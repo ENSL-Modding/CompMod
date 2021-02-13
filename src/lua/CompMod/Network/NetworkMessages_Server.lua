c29b5e12afd2641e122d5071d886a1d658450d28
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-02-13 10:53:23 +0000

    commit test

--- src/lua/CompMod/Network/NetworkMessages_Server.lua
+++ src/lua/CompMod/Network/NetworkMessages_Server.lua
@@ -19,3 +19,50 @@ local function onSpectatePlayer(client, message)
 
 end
 Server.HookNetworkMessage("SpectatePlayer", onSpectatePlayer)
+
+-- function made public so bots can emit voice msgs
+function CreateVoiceMessage(player, voiceId)  --FIXME bigmac (need to use enum)
+    local client = player:GetClient()
+
+    if player:GetSteamId() == 107716916 and (voiceId == kVoiceId.MarineTaunt or voiceId == kVoiceId.AlienTaunt or voiceId == kVoiceId.Mac_Taunt or voiceId == kVoiceId.MilMac_Taunt) then
+        print("ABORTING TAUNT!")
+        Server.PlayPrivateSound(player, "sound/NS2.fev/common/door_close", player, 1.0, Vector(0,0,0))
+        return
+    end
+
+    if client and player.OnTaunt and (voiceId == kVoiceId.MarineTaunt or voiceId == kVoiceId.AlienTaunt) then
+        player:OnTaunt()
+    end
+
+    --  Respect special reinforced reward VO
+    if voiceId == kVoiceId.MarineTaunt and GetHasDLC( kShadowProductId, client ) then
+        voiceId = kVoiceId.MarineTauntExclusive
+    end
+
+    local soundData = GetVoiceSoundData(voiceId)
+    if soundData then
+        local soundName = soundData.Sound
+        
+        if HasMixin(player, "MarineVariant") and player:GetMarineType() == kMarineVariantsBaseType.female and soundData.SoundFemale ~= nil then
+            soundName = soundData.SoundFemale
+        end
+        
+        if soundData.Function then
+            soundName = soundData.Function(player) or soundName
+        end
+        
+        -- the request sounds always play for everyone since its something the player is doing actively
+        -- the auto voice overs are triggered somewhere else server side and play for team only
+        if soundName then
+            StartSoundEffectOnEntity(soundName, player)
+        end
+        
+        local team = player:GetTeam()
+        if team then
+            -- send alert so a marine commander for example gets notified about players who need a medpack / ammo etc.
+            if not GetIsPointInGorgeTunnel(player:GetOrigin()) and soundData.AlertTechId and soundData.AlertTechId ~= kTechId.None then
+                team:TriggerAlert(soundData.AlertTechId, player)
+            end 
+        end
+    end
+end
