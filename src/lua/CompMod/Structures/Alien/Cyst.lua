0828433f8b780d7769c2837c6dc9d13ead21de0f
adsfgg <11364579+adsfgg@users.noreply.github.com>
2021-03-06 15:03:24 +0000

    Increase build rate on shift hive by 20%

--- src/lua/CompMod/Structures/Alien/Cyst.lua
+++ src/lua/CompMod/Structures/Alien/Cyst.lua
@@ -5,3 +5,12 @@ function Cyst:GetIsFlameableMultiplier(doer)
         return 7
     end
 end
+
+function Cyst:GetAutoBuildRateMultiplier()
+    if GetHasTech(self, kTechId.ShiftHive) then
+        -- return 1.25
+        return 1.5 -- increase by 20% from vanilla
+    end
+
+    return 1
+end
