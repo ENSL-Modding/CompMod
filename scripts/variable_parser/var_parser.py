658c2be0169056660f264c24bc807e24eedf41c8
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-01-16 21:59:42 +0000

    Print warning if var is redefined in local Balance.lua

--- scripts/variable_parser/var_parser.py
+++ scripts/variable_parser/var_parser.py
@@ -2,7 +2,7 @@ import re
 
 re_comments = re.compile("--.*\n")
 
-def parse_file(filepath : str, tokens : dict = None):
+def parse_file(filepath : str, tokens : dict = None, local_file : bool = False):
     if not tokens:
         tokens = dict()
 
@@ -44,6 +44,8 @@ def parse_file(filepath : str, tokens : dict = None):
 
         if not name in tokens:
             tokens[name] = value
+        elif local_file:
+            print("Warning: {} is already defined".format(name))
 
         # Set the end of the string to the start of our name minus the space
         end = name_start - 1
@@ -55,7 +57,7 @@ def parse_file(filepath : str, tokens : dict = None):
 
 
 def parse_local_and_vanilla(local_balance_filepath : str, vanilla_balance_filepath : str, vanilla_balance_health_filepath : str, vanilla_balance_misc_filepath : str):
-    local_tokens = parse_file(local_balance_filepath)
+    local_tokens = parse_file(local_balance_filepath, local_file=True)
     vanilla_tokens = parse_file(vanilla_balance_health_filepath)
     vanilla_tokens = parse_file(vanilla_balance_misc_filepath, tokens = vanilla_tokens)
     vanilla_tokens = parse_file(vanilla_balance_filepath, tokens = vanilla_tokens)
