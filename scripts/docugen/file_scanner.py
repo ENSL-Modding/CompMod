b8653c6dbcedd52e9338f01b0a827ef09c91ed0c
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-01-21 01:44:37 +0000

    Refactor docugen formatting

--- scripts/docugen/file_scanner.py
+++ scripts/docugen/file_scanner.py
@@ -2,11 +2,27 @@ import os
 import re
 from .verbose import verbose_print
 from variable_parser import var_parser
+from enum import Enum
+
+class FormatType (Enum):
+    NONE = 0
+    PERCENTAGE = 1
+    INVERSE_PERCENTAGE = 2
+    SCALAR = 3
+    DAMAGE_TYPE = 4
+
+format_type_map = {
+    "Percent": FormatType.PERCENTAGE,
+    "InvPercent": FormatType.INVERSE_PERCENTAGE,
+    "Scalar": FormatType.SCALAR,
+    "DamageType": FormatType.DAMAGE_TYPE
+}
 
 def process_key(c, key, key_data, mod_version, beta_version):
     if len(key_data) > 0 and key:
         insert_to_database(c, mod_version, beta_version, key, key_data)
         verbose_print(" -> Processed key: {}".format(key))
+
 
 def insert_to_database(c, mod_version, beta_version, key, key_data):
     for value in key_data:
@@ -15,9 +31,9 @@ def insert_to_database(c, mod_version, beta_version, key, key_data):
         else:
             c.execute("INSERT INTO FullChangelog(modVersion, key, value) VALUES (?,?,?)", [mod_version, key, value.strip()])
 
+
 def scan_for_docugen_files(conn, c, mod_version, beta_version, local_src_path, vanilla_src_path, local_balance_filepath, vanilla_balance_filepath, vanilla_balance_health_filepath, vanilla_balance_misc_filepath):
     local_tokens, vanilla_tokens = var_parser.parse_local_and_vanilla(local_balance_filepath, vanilla_balance_filepath, vanilla_balance_health_filepath, vanilla_balance_misc_filepath)
-
 
     # Delete any current entries for mod_version
     if beta_version > 0:
@@ -50,7 +66,9 @@ def scan_for_docugen_files(conn, c, mod_version, beta_version, local_src_path, v
                         key = line[1:].strip()
                         key_data = []
                     else:
-                        key_entry = process_key_entry(line.strip(), local_tokens, vanilla_tokens, local_src_path, vanilla_src_path)
+                        key_entry = line.strip()
+                        key_entry = process_dynamic_var(key_entry, local_tokens, local_src_path)
+                        key_entry = process_generated_statement(key_entry, local_tokens, vanilla_tokens, local_src_path, vanilla_src_path)
                         key_data.append(key_entry)
                 
                 # Process the last key if there is one
@@ -60,154 +78,121 @@ def scan_for_docugen_files(conn, c, mod_version, beta_version, local_src_path, v
     conn.commit()
 
 
-re_dynamic_vars = re.compile("\{\{([^ ]+(?:, *[^ ]+ *= *[^$,]+)*)\}\}")
-re_generated_statements = re.compile("^(>*)!(.*)$")
-def process_key_entry(key_entry : str, local_tokens : dict, vanilla_tokens : dict, local_src_path : str, vanilla_src_path : str):
-    dynamic_vars = re_dynamic_vars.findall(key_entry)
-    if dynamic_vars:
-        return process_dynamic_var(key_entry, dynamic_vars, local_tokens, vanilla_tokens, local_src_path)
-    
-    m = re_generated_statements.search(key_entry)
-    if m:
-        (indent, s) = m.groups()
-        return process_generated_statement(indent, s, local_tokens, vanilla_tokens, local_src_path, vanilla_src_path)
-
-    return key_entry
-
-
+re_dynamic_vars = re.compile("\{\{([^ ]+(?:, *[^ ]+ *= *[^$,}]+)*)\}\}")
 
 # Replace dynamic vars with their values
-def process_dynamic_var(key_entry : str, dynamic_vars : list, local_tokens : dict, vanilla_tokens : dict, local_src_path : str):
+def process_dynamic_var(key_entry : str, local_tokens : dict, local_src_path : str):
+    dynamic_vars = re_dynamic_vars.findall(key_entry)
     for s in dynamic_vars:
         value = None
-        source = None
+        fmt = None
+        var = None
+
         if s.find(",") != -1:
             var = s[0:s.index(",")]
-            for m in re.finditer("([^ ]+)=([^,$]*)(?=,|$)", s):
-                (name,value) = m.groups()
-                
-                if name == "format":
-                    fmt = value
-                if name == "source":
-                    source = value.lower()
-
-            if var.find(":") != -1:
-                (filename, varname) = var.split(":")
-
-                value = find_val_in_file(filename, varname, local_src_path)
-            else:
-                if source:
-                    if source == "vanilla":
-                        value = vanilla_tokens[var]
-                    elif source == "compmod":
-                        value = local_tokens[var]
-                else:
-                    value = local_tokens[var]
-
-            if fmt == "Scalar":
-                value = float(value) - 1
-                fmt = "%"
-
-            if fmt == "%":
-                value = str(round(float(value) * 100, 2)) + "%"
-
-            if fmt == "DamageType":
-                value = value.replace("kDamageType.", "")
+            args = parse_args(s, ["format"])
+            fmt = get_format_type(args.get("format"))
         else:
-            if s.find(":") != -1:
-                (filename, varname) = s.split(":")
+            var = s
 
-                value = find_val_in_file(filename, varname, local_src_path)
-            else:
-                if source:
-                    if source == "vanilla":
-                        value = vanilla_tokens[s]
-                    elif source == "compmod":
-                        value = local_tokens[s]
-                else:
-                    value = local_tokens[s]
+        value = resolve_variable(var, local_tokens, local_src_path)
+        value = transform_value(value, fmt)
+        value = format_value(value, fmt)
 
         key_entry = key_entry.replace("{{{{{}}}}}".format(s), value)
     
     return key_entry
 
-def process_generated_statement(indent : str, s : str, local_tokens : dict, vanilla_tokens : dict, local_src_path : str, vanilla_src_path : str):
-    desc = None
-    fmt = None
-    suffix = ""
-    suffix_singular = None
-    additional_lookup = None
 
+re_generated_statements = re.compile("^(>*)!(.*)$")
+def process_generated_statement(key_entry : str, local_tokens : dict, vanilla_tokens : dict, local_src_path : str, vanilla_src_path : str):
+    m = re_generated_statements.search(key_entry)
+    if not m: return key_entry
+
+    (indent, s) = m.groups()
     var = s[0:s.index(",")]
-    for m in re.finditer("([^ ]+)=([^,$]*)(?=,|$)", s):
-        (name,value) = m.groups()
-        
-        if name == "description":
-            desc = value
-        elif name == "format":
-            fmt = value
-        elif name == "suffix":
-            suffix = value
-        elif name == "suffix_singular":
-            suffix_singular = value
-        elif name == "additional_lookup":
-            additional_lookup = value.lower()
+    args = parse_args(s, [
+        "description",
+        "format",
+        "suffix",
+        "suffix_singular"
+    ])
+    desc = args.get("description")
+    fmt = get_format_type(args.get("format"))
+    suffix = args.get("suffix", "")
+    suffix_singular = args.get("suffix_singular")
 
     if suffix_singular and not suffix:
         raise Exception("Must provide suffix when using suffix_singular")
 
-    to_val = None
-    from_val = None
-
-    if var.find(":") != -1:
-        (filename, varname) = var.split(":")
-
-        to_val = find_val_in_file(filename, varname, local_src_path)
-        from_val = find_val_in_file(filename, varname, vanilla_src_path)
-    else:
-        to_val = local_tokens[var]
-        from_val = vanilla_tokens[var]
-
-    if additional_lookup == "vanilla":
-        from_val = vanilla_tokens[from_val]
-    elif additional_lookup == "compmod":
-        to_val = local_tokens[to_val]
-    elif additional_lookup:
-        raise Exception("Invalid additional_lookup")
+    to_val = resolve_variable(var, local_tokens, local_src_path)
+    from_val = resolve_variable(var, vanilla_tokens, vanilla_src_path)
 
     # Perform any value modifications here before we figure out the verb
-    if fmt == "-%":
-        to_val = 1 - float(to_val)
-        from_val = 1 - float(from_val)
-        fmt = "%"
-
-    if fmt == "Scalar":
-        to_val = float(to_val) - 1
-        from_val = float(from_val) - 1
-        fmt = "%"
+    to_val = transform_value(to_val, fmt)
+    from_val = transform_value(from_val, fmt)
 
     verb = "Decreased" if to_val < from_val else "Increased"
 
-    if fmt == '%':
-        to_val = str(round(float(to_val) * 100, 2)) + "%"
-        from_val = str(round(float(from_val) * 100, 2)) + "%"
-
-    if fmt == "DamageType":
-        to_val = to_val.replace("kDamageType.", "")
-        from_val = from_val.replace("kDamageType.", "")
-
-    to_suffix = suffix
-    from_suffix = suffix
+    to_val = format_value(to_val, fmt)
+    from_val = format_value(from_val, fmt)
 
     if to_val.isdigit() and int(to_val) == 1:
         to_suffix = suffix_singular
+    else:
+        to_suffix = suffix
 
     if from_val.isdigit() and int(from_val) == 1:
         from_suffix = suffix_singular
+    else:
+        from_suffix = suffix
 
-    to_suffix_space = " " if len(to_suffix) > 0 else ""
-    from_suffix_space = " " if len(from_suffix) > 0 else ""
+    to_suffix_space = " " if to_suffix and len(to_suffix) > 0 else ""
+    from_suffix_space = " " if from_suffix and len(from_suffix) > 0 else ""
+    
     return "{}{} {} to {}{}{} from {}{}{}".format(indent, verb, desc, to_val, to_suffix_space, to_suffix, from_val, from_suffix_space, from_suffix)
+
+
+re_additional_args = re.compile("([^ ]+)=([^,$]*)(?=,|$)")
+def parse_args(s : str, valid_args : list):
+    additional_args = dict()
+
+    for m in re_additional_args.finditer(s):
+        (name,value) = m.groups()
+        
+        if name not in valid_args:
+            raise Exception("Invalid argument \"{}\"".format(name))
+
+        additional_args[name] = value
+    
+    return additional_args
+
+
+def transform_value(val, fmt : FormatType):
+    if fmt == FormatType.INVERSE_PERCENTAGE:
+        return 1 - float(val)
+    elif fmt == FormatType.SCALAR:
+        return float(val) - 1
+    else:
+        return val
+
+
+def format_value(val, fmt : FormatType):
+    if fmt == FormatType.PERCENTAGE or fmt == FormatType.INVERSE_PERCENTAGE or fmt == FormatType.SCALAR:
+        return "{0:g}%".format(round(float(val) * 100, 2))
+    elif fmt == FormatType.DAMAGE_TYPE:
+        return val.replace("kDamageType.", "")
+    else:
+        return val
+
+
+def resolve_variable(var : str, tokens : dict, src_path : str):
+    if var.find(":") != -1:
+        (filename, varname) = var.split(":")
+
+        return find_val_in_file(filename, varname, src_path)
+    else:
+        return tokens[var]
 
 
 re_comments = re.compile("--.*$")
@@ -233,3 +218,7 @@ def find_val_in_file(filename : str, varname : str, src_path : str):
             return value
 
     return None
+
+
+def get_format_type(fmt : str):
+    return format_type_map.get(fmt, FormatType.NONE)
