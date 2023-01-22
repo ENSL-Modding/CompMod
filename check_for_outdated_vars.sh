2abcabafa4db598f32ee3b6b4e581e12edb322b6
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-01-22 11:57:41 +0000

    Add check_for_outdated_vars script

---
+++ check_for_outdated_vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+test $1 || { echo "Usage: $0 ns2_install_path"; exit 1; }
+install_path="$1"
+shift
+
+python3 scripts/var_checker.py src/lua/CompMod src/lua/CompMod/Globals/Balance.lua "$install_path/ns2/lua/Balance.lua" "$install_path/ns2/lua/BalanceHealth.lua" "$install_path/ns2/lua/BalanceMisc.lua"
