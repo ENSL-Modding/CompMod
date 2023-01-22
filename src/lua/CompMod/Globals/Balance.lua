fc0ca6479846f615cc9fb727d9b445ddcbf1c7b8
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-01-22 12:12:43 +0000

    Remove unused Shotgun vars

--- src/lua/CompMod/Globals/Balance.lua
+++ src/lua/CompMod/Globals/Balance.lua
@@ -71,13 +71,6 @@ kSentryCost = 6 -- 5
 
 kSentryBatteryCost = 12 -- 10
 
--- Increase damage per upgrade to ~13.33 from ~10
-
-local kShotgunDamagePerUpgradeScalar = 0.078
-kShotgunWeapons1DamageScalar = 1 + kShotgunDamagePerUpgradeScalar
-kShotgunWeapons2DamageScalar = 1 + kShotgunDamagePerUpgradeScalar * 2
-kShotgunWeapons3DamageScalar = 1 + kShotgunDamagePerUpgradeScalar * 3
-
 kObservatorySupply = 30 -- 25
 kRoboticsFactorySupply = 15 --5
 
