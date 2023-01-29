f68c82ec76d7484abf6bb4b755e174145ff62dcf
adsfgg <11364579+adsfgg@users.noreply.github.com>
2023-01-29 01:40:46 +0000

    Remove change to prevent hallucinations from healing; in vanilla

--- src/lua/CompMod/Mixins/PlayerHallucinationMixin.lua
+++ src/lua/CompMod/Mixins/PlayerHallucinationMixin.lua
@@ -1,9 +1,5 @@
 if not Server then return end
 
-function PlayerHallucinationMixin:ModifyHeal(healTable)
-    healTable.health = 0
-end
-
 function PlayerHallucinationMixin:GetHealthPerBioMass()
     return 0
 end
